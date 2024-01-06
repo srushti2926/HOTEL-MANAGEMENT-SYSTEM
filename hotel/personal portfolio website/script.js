8d849054109056cd13982ae7556b3f185361e151
Srushti Jagtap <86220166+srushti2926@users.noreply.github.com>
2024-01-06 13:42:47 +0530

    Add files via upload

---
+++ hotel/personal portfolio website/script.js
@@ -0,0 +1,11 @@
+function showResume() {
+    alert('This is your resume. Update this function with your resume display logic.');
+}
+
+function showProjects() {
+    alert('Redirecting to Projects page.');
+}
+
+function showAbout() {
+    alert('This is your About Me. Update this function with your about me display logic.');
+}
